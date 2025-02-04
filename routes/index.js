f205910f11c7659dfd9d6ec68a12e633e7e350c0
ToKolob <anolis27@hotmail.com>
2025-02-03 21:34:41 -0300

    ok

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<p>Hello! Welcome to Memorize Quotes API </p>' +
+    '<br> <a href="/api-docs">API Documentation</a>' +
+    '<br> <a href="/auth/google">Login with Google</a>');
 });
 
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
