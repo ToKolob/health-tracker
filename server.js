b81ddcf8aeb2c34b3e2b591388318c159b69cb1e
OhayitsFay <atu23001@byui.edu>
2025-02-10 22:51:15 -0600

    water and meals rest

--- server.js
+++ server.js
@@ -15,9 +15,6 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/index.js'));
 
 
-process.on('uncaughtException', (err) => {
-  console.log(process.stderr.fd, `Caught exception: ${err} \n Exception origin: ${origin}`);
-});
 
 mongodb.initDb((err, db) => {
   if (err) {
