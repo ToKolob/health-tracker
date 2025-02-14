33f54e43e3b2b934ffa6d56f9af851fbc6153ea0
7pigy7 <7pigy7@gmail.com>
2025-02-14 12:05:49 -0800

    exercies rest and minor fixes

--- routes/exercises.js
+++ routes/exercises.js
@@ -7,12 +7,9 @@ const { isAutehnticated } = require('../middleware/authenticate.js');
 
 router.get('/', controller.getAllExercises);
 router.get('/:id', controller.getByIDExercises);
-// router.get('/:name', controller.getByNameExercises);
-// router.get('/:date', controller.getByDateExercises);
-// router.get('/:duration', controller.getByDurationExercises);
 router.post('/', isAutehnticated, exerciseValidationRules(),  validate, controller.postExercise);
 router.put('/', isAutehnticated, exerciseValidationRules(), validate, controller.updateExercises);
-router.delete('/:id', controller.deleteExercises)
+router.delete('/:id', isAutehnticated, controller.deleteExercises)
 
 module.exports = router;
 
